f8dafa1c84ee93740578a3153a1100af2ebb1bce
Lucas PLE <plel@ayou07.iut-infobio.priv.univ-lille1.fr>
2020-09-29 08:30:26 +0200

    Point, face

---
+++ sources-du-projet/src/main/java/com/groupe5/geometry/Face.java
@@ -0,0 +1,11 @@
+package com.groupe5.geometry;
+
+public class Face {
+	private int nbPoints;
+	private Point[] points;
+	
+	public Face(int nbPoints, Point[] points) {
+		this.nbPoints = nbPoints;
+		this.points = points;
+	}
+}
