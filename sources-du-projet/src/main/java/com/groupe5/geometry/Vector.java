a16d37de773fabf8ca71f2c013f5f718e2c39e6f
Aubrian DUHAYON <duhayona@ayou08.iut-infobio.priv.univ-lille1.fr>
2020-09-29 08:25:27 +0200

    init vector

---
+++ sources-du-projet/src/main/java/com/groupe5/geometry/Vector.java
@@ -0,0 +1,24 @@
+package com.groupe5.geometry;
+
+import java.awt.Point;
+
+public class Vector {
+
+	private float x1, x2; //x1 -> Point de départ sur l'axe X / x2 -> Point d'arrivée sur l'axe X
+	private float y1, y2; //y1 -> Point de départ sur l'axe X / y2 -> Point d'arrivée sur l'axe X
+	private float z1, z2; //z1 -> Point de départ sur l'axe X / z2 -> Point d'arrivée sur l'axe X
+	
+	public Vector(Point p1, Point p2) {
+		
+		x1 = p1.getX();
+		y1 = p1.getY();
+		z1 = p1.getZ();
+		
+		x2 = p2.getX();
+		y2 = p2.getY();
+		z2 = p2.getZ();		
+	}
+	
+	public get 
+	
+}
