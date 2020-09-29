75e2fe8b481a867d797e93d7cbcefc16894c4a71
Axel Pirc <axel.pirc.etu@univ-lille.fr>
2020-09-29 09:04:13 +0200

    javafx

---
+++ sources-du-projet/src/main/java/com/groupe5/view/ShowScene.java
@@ -0,0 +1,26 @@
+package com.groupe5.view;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ShowScene extends Application{
+	public void start(Stage stage) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("scene.fxml"));
+        Parent root = loader.load();
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("FXML Demo");
+        stage.show();
+    }
+    
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+}
