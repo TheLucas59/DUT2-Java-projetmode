a03e52c81e1c5410150b240ccfd49b73e0580453
FruityEnLoops <fruityenloops@gmail.com>
2020-10-08 15:43:39 +0200

    Parser (separate file)

--- sources-du-projet/src/main/java/com/groupe5/geometry/Point.java
+++ sources-du-projet/src/main/java/com/groupe5/geometry/Point.java
@@ -4,8 +4,9 @@ public class Point {
 	private double x;
 	private double y;
 	private double z;
+	int id;
 	
-	public Point(double x, double y, double z) {
+	public Point(double x, double y, double z, int id) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
