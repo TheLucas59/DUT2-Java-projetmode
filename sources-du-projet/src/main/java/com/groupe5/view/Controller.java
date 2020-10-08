132ae07f521989c56563c2636a7ac6da8836c263
axel <axel@axel-VirtualBox>
2020-10-08 14:52:16 +0200

    controller + fxml

--- sources-du-projet/src/main/java/com/groupe5/view/Controller.java
+++ sources-du-projet/src/main/java/com/groupe5/view/Controller.java
@@ -3,17 +3,20 @@ package com.groupe5.view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 
 public class Controller {
 
 	@FXML Canvas canvas;
+	@FXML Button buttonOpen;
+	@FXML Button buttonClose;
 	
 	public void buttonOpenFile(ActionEvent e){
-		
+		System.out.println("aaaaaa");
 	}
 	
 	public void buttonCloseFile(ActionEvent e){
-		
+		System.out.println("bbbbbb");
 	}
 	
 }
