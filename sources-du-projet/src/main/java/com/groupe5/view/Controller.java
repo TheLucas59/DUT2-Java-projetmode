27e4939fcd405e8d5e2d81eb832f44bdc26b0b12
Axel <axel.pirc.etu@univ-lille.fr>
2020-10-08 14:23:41 +0200

    fxml + controller

---
+++ sources-du-projet/src/main/java/com/groupe5/view/Controller.java
@@ -0,0 +1,19 @@
+package com.groupe5.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+public class Controller {
+
+	@FXML Canvas canvas;
+	
+	public void buttonOpenFile(ActionEvent e){
+		
+	}
+	
+	public void buttonCloseFile(ActionEvent e){
+		
+	}
+	
+}
