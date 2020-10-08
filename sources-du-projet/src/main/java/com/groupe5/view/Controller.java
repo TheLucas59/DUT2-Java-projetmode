174b16dde3972bf2e073b887f5d3a4865da0867c
Aubrian <aubriduhay@gmail.com>
2020-10-08 14:56:05 +0100

    merge

--- sources-du-projet/src/main/java/com/groupe5/view/Controller.java
+++ sources-du-projet/src/main/java/com/groupe5/view/Controller.java
@@ -3,17 +3,46 @@ package com.groupe5.view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ListView;
+import javafx.stage.Popup;
+import javafx.stage.PopupWindow.AnchorLocation;
+
 
 public class Controller {
 
 	@FXML Canvas canvas;
+
+	Popup p;
+	
+	ListView<String> plyFiles;
+	
+	final String PATH = "./exemples";
+	
+	
+	public void initialize() {
+		
+	}
+	
 	
 	public void buttonOpenFile(ActionEvent e){
+
+		p = new Popup();
+
+		ListPLY files = new ListPLY(PATH);
+		plyFiles = new ListView<String>(files.getFiles());
+
+		p.getContent().add(plyFiles);
+		
+		p.setAnchorLocation(AnchorLocation.CONTENT_TOP_RIGHT);
+		
+		p.setAutoHide(true);
+
+		p.show(canvas.getParent().getScene().getWindow(), p.getAnchorX(), p.getAnchorY());
 		
 	}
 	
 	public void buttonCloseFile(ActionEvent e){
-		
+		p.hide();
 	}
 	
 }
