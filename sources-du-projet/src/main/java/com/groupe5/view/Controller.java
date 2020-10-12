c3b9fa3db29d297daa51f391417f5e88c4e845f5
Aubrian <aubriduhay@gmail.com>
2020-10-12 15:03:21 +0100

    Thread

--- sources-du-projet/src/main/java/com/groupe5/view/Controller.java
+++ sources-du-projet/src/main/java/com/groupe5/view/Controller.java
@@ -1,20 +1,21 @@
 package com.groupe5.view;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.groupe5.parser.Parser;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.NodeOrientation;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.CullFace;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
-import javafx.scene.shape.VertexFormat;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
-
-import java.io.File;
-import java.io.IOException;
 
 public class Controller {
 
@@ -22,16 +23,21 @@ public class Controller {
 	@FXML Button buttonOpen;
 	@FXML Button buttonClose;
 	@FXML AnchorPane root;
+	@FXML Text loadingString;
+	@FXML ProgressBar loadingBar;
+	@FXML MenuBar menuBar;
 
 	final String PATH = "./exemples/";
 
 	public void initialize(){
 		meshView.setOnScroll(scroll -> {
+			
+			System.out.println(meshView.getScaleX() + " - " + meshView.getScaleY());
 			if(scroll.getDeltaY() > 0) {
 				meshView.setScaleX(meshView.getScaleX()+1);
 				meshView.setScaleY(meshView.getScaleY()+1);
 			}
-			else if(scroll.getDeltaY() < 0) {
+			else if(scroll.getDeltaY() < 0 && meshView.getScaleX() > 1 && meshView.getScaleY() > 1) {
 				meshView.setScaleX(meshView.getScaleX()-1);
 				meshView.setScaleY(meshView.getScaleY()-1);
 			}
@@ -59,36 +65,52 @@ public class Controller {
 			@Override
 			public void run() {
 				Parser p = null;
+				
+				loadingString.setVisible(true);
+				loadingBar.setVisible(true);
 
 				try {
 					p = new Parser(fileToShow);
 				} catch (IOException ioException) {}
+				
+				loadingBar.setProgress(25);
 
 
 				TriangleMesh mesh = new TriangleMesh();
 
 				mesh.getTexCoords().addAll(0, 0);
+				
+				loadingBar.setProgress(50);
 
 				// Side
 
 				for (int i = 0; i < p.getPoints().size(); i++) {
 					mesh.getPoints().addAll(p.getPoints().get(i).getX(), p.getPoints().get(i).getY(), p.getPoints().get(i).getZ());
 				}
+				
+				loadingBar.setProgress(75);
 
 				p.getFaces(p.getPoints());
 				for (int i = 0; i < p.getIdPoints().length; i++) {
 					int[] idPoint = p.getIdPoints();
 					mesh.getFaces().addAll(idPoint[i],0);
 				}
+				
+				loadingBar.setProgress(100);
+				
 				meshView.setMesh(mesh);
+				
+				loadingString.setVisible(false);
+				loadingBar.setVisible(false);
 			}
 		});
 
 		meshView.setDrawMode(DrawMode.FILL);
 		meshView.setTranslateX(318);
 		meshView.setTranslateY(120);
+		meshView.setScaleX(2);
+		meshView.setScaleY(2);
 
-		showMesh.setDaemon(true);
 		showMesh.start();
 	}
 	
