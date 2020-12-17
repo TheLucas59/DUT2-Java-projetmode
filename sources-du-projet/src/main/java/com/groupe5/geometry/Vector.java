b4fd0268656a611a783770552d633c5af1771deb
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2020-12-17 09:24:03 +0100

    Change class Vector

--- sources-du-projet/src/main/java/com/groupe5/geometry/Vector.java
+++ sources-du-projet/src/main/java/com/groupe5/geometry/Vector.java
@@ -2,26 +2,39 @@ package com.groupe5.geometry;
 
 public class Vector {
 
-	private float x1, x2; //x1 -> Point de départ sur l'axe X / x2 -> Point d'arrivée sur l'axe X
-	private float y1, y2; //y1 -> Point de départ sur l'axe X / y2 -> Point d'arrivée sur l'axe X
-	private float z1, z2; //z1 -> Point de départ sur l'axe X / z2 -> Point d'arrivée sur l'axe X
+	private float x;
+	private float y;
+	private float z;
 	
 	public Vector(Point p1, Point p2) {		
-		x1 = p1.getX();
-		y1 = p1.getY();
-		z1 = p1.getZ();
+		x = p1.getX() - p2.getX();
+		y = p1.getY() - p2.getY();
+		z = p1.getZ() - p2.getZ();
 		
-		x2 = p2.getX();
-		y2 = p2.getY();
-		z2 = p2.getZ();		
+	}
+	
+	public Vector(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 	
 	public float[] getVector() {
-		return new float[] {x1, y1, z1, x2, y2, z2};
+		return new float[] {x, y, z};
 	}
 	
-	public float[] getEndPoint() {
-		return new float[] {x2, y2, z2};
+	public Vector produitVectoriel(Vector other) {
+		float newx = this.y * other.z - other.y * this.z;
+		float newy = this.z * other.x - other.z * this.x;
+		float newz = this.x * other.y - other.x * this.y;
+		return new Vector(newx, newy, newz);
+	}
+	
+	public float produitScalaire(Vector other) {
+		float newx = this.x * other.y;
+		float newy = this.y * other.y;
+		float newz = this.z * other.z;
+		return newx + newy + newz;
 	}
 	
 }
